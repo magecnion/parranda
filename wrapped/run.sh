cd6e1a3477869863489a0ad83b0b1a83ef8f210d
magecnion <magecnion@gmail.com>
2026-07-04 11:06:23 +0100

    update wrapped script to include rust and fix some node insconsistences

--- wrapped/run.sh
+++ wrapped/run.sh
@@ -3,10 +3,15 @@ set -euo pipefail
 
 PROJECT=$(realpath "$1")
 SANDBOX_HOME="$HOME/.local/share/opencode-sandbox/home"
-NODE_BIN_DIR=$(dirname "$(command -v pnpm)")
+# NODE_BIN_DIR=$(dirname "$(command -v pnpm)")
+  # --ro-bind "$NODE_BIN_DIR" /home/sandbox/.local/node-bin \
 PNPM_HOME="$HOME/.local/share/pnpm"
+NODE_PREFIX=$(dirname "$(dirname "$(readlink -f "$(command -v node)")")")
+CARGO_HOME="$HOME/.cargo"
+RUSTUP_HOME="$HOME/.rustup"
+NPX_HOME="$HOME/.nvm/versions/node/v24.16.0/bin/npx"
 
-mkdir -p "$SANDBOX_HOME" "$SANDBOX_HOME/.local/node-bin" "$SANDBOX_HOME/.local/share/pnpm"
+mkdir -p "$SANDBOX_HOME" "$SANDBOX_HOME/.local/share/pnpm"
 
 exec bwrap \
   --unshare-all \
@@ -33,10 +38,12 @@ exec bwrap \
   --bind "$PROJECT" /working-dic \
   --bind "$SANDBOX_HOME" /home/sandbox \
   --ro-bind "$HOME/.opencode/bin" /home/sandbox/.opencode/bin \
-  --ro-bind "$NODE_BIN_DIR" /home/sandbox/.local/node-bin \
+  --ro-bind "$CARGO_HOME" /home/sandbox/.cargo \
+  --ro-bind "$RUSTUP_HOME" /home/sandbox/.rustup \
   --ro-bind "$PNPM_HOME" /home/sandbox/.local/share/pnpm \
-  --ro-bind "$PROJECT/wrapped/AGENTS.md" /home/sandbox/.config/opencode/AGENTS.md \
-  --ro-bind "$PROJECT/wrapped/opencode.jsonc" /home/sandbox/.config/opencode/opencode.jsonc \
+  --ro-bind "$NODE_PREFIX" /home/sandbox/.local/node \
+  --ro-bind-try "$PROJECT/wrapped/AGENTS.md" /home/sandbox/.config/opencode/AGENTS.md \
+  --ro-bind-try "$PROJECT/wrapped/opencode.jsonc" /home/sandbox/.config/opencode/opencode.jsonc \
   --ro-bind "$OC_SKILLS" /home/sandbox/.config/opencode/skills \
   --ro-bind "$OC_AGENTS" /home/sandbox/.config/opencode/agents \
   --ro-bind "$OC_COMMANDS" /home/sandbox/.config/opencode/commands \
@@ -46,7 +53,9 @@ exec bwrap \
   --setenv HOME /home/sandbox \
   --setenv USER sandbox \
   --setenv OF_ROOT /home/sandbox/openFrameworks \
-  --setenv PATH /home/sandbox/.local/node-bin:/home/sandbox/.local/share/pnpm:/home/sandbox/.local/share/pnpm/bin:/home/sandbox/.opencode/bin:/usr/local/bin:/usr/bin:/bin \
+  --setenv CARGO_HOME /home/sandbox/.cargo \
+  --setenv RUSTUP_HOME /home/sandbox/.rustup \
+  --setenv PATH /home/sandbox/.cargo/bin:/home/sandbox/.local/node/bin:/home/sandbox/.local/share/pnpm:/home/sandbox/.local/share/pnpm/bin:/home/sandbox/.opencode/bin:/usr/local/bin:/usr/bin:/bin \
   \
   --setenv TERM "$TERM" \
   --setenv COLORTERM "$COLORTERM" \
